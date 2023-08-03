d34b90dd3dda8b1610308b8a71164b55eeca6389
Adityakitukale49 <adityakitukale4599@gmail.com>
2023-08-03 15:51:38 +0530

    done

---
+++ src/Components/CommunitiiesOption.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './RightSection.css'
+const CommunitiiesOption = () => {
+  return (
+    <div><div className="top">
+    <div className='community'>
+        <img src="dsaImg.jpeg" className='Logo' />
+        <div className='members'>
+            <h5>DSA Community</h5>
+            <p><small> 114 Members</small></p>
+        </div>
+    </div>
+    <div className='join'>
+        <i class="fa-solid fa-plus"></i>
+        <span> Join</span>
+    </div>
+</div>
+</div>
+  )
+}
+
+export default CommunitiiesOption
--- src/Components/RightSection.jsx
+++ src/Components/RightSection.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './RightSection.css'
+import CommunitiiesOption from './CommunitiiesOption'
 const RightSection = () => {
   return (
     <div className='RightSection'>
         <div className="search">
         <input type="text" className='inp'/> <i class="fa-solid fa-magnifying-glass searchIcon"></i>
         </div>
+        <p>Popular Communities</p>
+        <CommunitiiesOption/>
     </div>
   )
 }
